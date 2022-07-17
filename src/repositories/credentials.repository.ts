5a8d9d878fe435e8391c0412508be7bcdc309c47
santannaizadora <santannaizadora@gmail.com>
2022-07-17 18:25:05 -0300

    feat: delete credential

--- src/repositories/credentials.repository.ts
+++ src/repositories/credentials.repository.ts
@@ -12,6 +12,27 @@ const findCredentialByUserAndTitle = async (userId: number, title: string) => {
     });
 }
 
+const findCredentialByIdAndUser = async (id: number, user_id: number) => {
+    return await client.credential.findFirst({
+        where: {
+            id,
+            user: {
+                id: user_id,
+            },
+        },
+    });
+}
+
+const findUserCredentials = async ( user_id: number ) => {
+    return await client.credential.findMany({
+        where: {
+            user: {
+                id: user_id,
+            },
+        },
+    });
+}
+
 const insert = async (credential: CreateCredentialData) => {
     await client.credential.create({
         data: {
@@ -28,7 +49,18 @@ const insert = async (credential: CreateCredentialData) => {
     });
 }
 
+const delet = async (id: number) => {
+    await client.credential.delete({
+        where: {
+            id,
+        },
+    });
+}
+
 export const credentialRepository = {
     findCredentialByUserAndTitle,
     insert,
+    findCredentialByIdAndUser,
+    findUserCredentials,
+    delet,
 }
--- src/services/credentials.service.ts
+++ src/services/credentials.service.ts
@@ -3,10 +3,26 @@ import { credentialRepository } from "../repositories/credentials.repository.js"
 
 export type CreateCredentialData = Partial<Credential>;
 
-const createCredential = async (data: CreateCredentialData) => {
+const findCredentialForUser = async (user_id: number, id: number) => {
+    const credential = await credentialRepository.findCredentialByIdAndUser(
+        id,
+        user_id
+    );
+
+    if (!credential) {
+        throw {
+            type: "not_found",
+            message: "credential does not belong or doesn't exist for this user",
+        };
+    }
+
+    return credential;
+}
+
+const titleIsAvailable = async (title: string, user_id: number) => {
     const credential = await credentialRepository.findCredentialByUserAndTitle(
-        data.user_id,
-        data.title
+        user_id,
+        title
     );
     
     if (credential) {
@@ -15,10 +31,31 @@ const createCredential = async (data: CreateCredentialData) => {
             message: "You already have a credential with this title",
         };
     }
+}
 
+const createCredential = async (data: CreateCredentialData) => {
+    await titleIsAvailable(data.title, data.user_id);
     await credentialRepository.insert(data);
+}
+
+const findOne = async (id: number, user_id: number) => {
+    const credential = await findCredentialForUser(user_id, id);
+    return credential;
+}
+
+const findAll = async (user_id: number) => {
+    const credentials = await credentialRepository.findUserCredentials(user_id);
+    return credentials;
+}
+
+const deleteOne = async (id: number, user_id: number) => {
+    await findCredentialForUser(user_id, id);
+    await credentialRepository.delet(id);
 }
 
 export const credentialsService = {
     createCredential,
+    findOne,
+    findAll,
+    deleteOne,
 }
