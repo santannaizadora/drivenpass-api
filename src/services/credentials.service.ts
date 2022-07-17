998a75654e6017760ce8b3894a8ebdbd3762fc66
santannaizadora <santannaizadora@gmail.com>
2022-07-17 18:31:23 -0300

    fix: encryp passwords

--- src/services/credentials.service.ts
+++ src/services/credentials.service.ts
@@ -1,5 +1,8 @@
 import { Credential } from "@prisma/client";
 import { credentialRepository } from "../repositories/credentials.repository.js";
+import Cryptr from "cryptr";
+
+const cryptr = new Cryptr(process.env.ENCRYPTION_KEY);
 
 export type CreateCredentialData = Partial<Credential>;
 
@@ -35,16 +38,24 @@ const titleIsAvailable = async (title: string, user_id: number) => {
 
 const createCredential = async (data: CreateCredentialData) => {
     await titleIsAvailable(data.title, data.user_id);
-    await credentialRepository.insert(data);
+    const hashedPassword = cryptr.encrypt(data.password);
+    await credentialRepository.insert({
+        ...data,
+        password: hashedPassword,
+    });
 }
 
 const findOne = async (id: number, user_id: number) => {
     const credential = await findCredentialForUser(user_id, id);
+    credential.password = cryptr.decrypt(credential.password);
     return credential;
 }
 
 const findAll = async (user_id: number) => {
     const credentials = await credentialRepository.findUserCredentials(user_id);
+    credentials.forEach((credential) => {
+        credential.password = cryptr.decrypt(credential.password);
+    });
     return credentials;
 }
 
